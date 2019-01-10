0812094f1707b42244a7b9540562ad74877a0cff
msconverse1 <msconverse@fullsail.edu>
2019-01-10 13:03:00 -0600

    Created a Set and Get for The CustomList for the Length and the Indexer+ Created Basic Add to List

--- MSC_CustomListClass/ListTest/UnitTest1.cs
+++ MSC_CustomListClass/ListTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MSC_CustomListClass;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ListTest
@@ -7,8 +8,63 @@ namespace ListTest
     public class UnitTest1
     {
         [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            CustomList<int> intList = new CustomList<int>();
+            int expectedResult = 5;
+            int expectedResult1 = 7;
+            int expectedCount = 2;
+            //Act
+            intList.Add(expectedResult);
+            intList.Add(expectedResult1);
+
+            //Assert
+            Assert.AreEqual(expectedCount, intList.Count);
+        }
+
+
+        [TestMethod]
         public void TestMethod1()
         {
+            //Arrange
+            CustomList<int> intList = new CustomList<int>();
+            int expectedResult = 5;
+            //Act
+            intList.Add(expectedResult);
+
+            //Assert
+            Assert.AreEqual(expectedResult, intList[0]);
         }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            CustomList<int> intList = new CustomList<int>();
+            int expectedResult = 5;
+            int expectedResult1 = 7;
+            //Act
+            intList.Add(expectedResult);
+            intList.Add(expectedResult1);
+
+            //Assert
+            Assert.AreEqual(expectedResult, intList[0]);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            CustomList<int> intList = new CustomList<int>();
+            int expectedResult = 5;
+            int expectedResult1 = 7;
+            //Act
+            intList.Add(expectedResult);
+            intList.Add(expectedResult1);
+
+            //Assert
+            Assert.AreEqual(expectedResult1, intList[1]);
+        }
+       
+
     }
 }
---
+++ MSC_CustomListClass/MSC_CustomListClass/CustomList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSC_CustomListClass
+{
+    public class CustomList<T>
+    {
+
+        T[] items = new T[0];
+        private int _count;
+        //Count for the Arrays current size
+       public int Count { get => _count; set => _count = value; }
+        //Idexer declaration 
+        public T this[int index] { get => items[index]; set => items[index] = value; }
+        public void Add(T item)
+        {
+            Count = items.Length + 1;
+            Array.Resize(ref items, Count);
+            items[items.Length - 1] = item;  
+        }
+
+    }
+}
